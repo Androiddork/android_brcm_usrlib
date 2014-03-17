0eadb508099008e1280e540a0c0d0346cf2bf1e1
Viktor Warg <viktor.warg@gmail.com>
2014-03-17 09:28:27 +0100

    Update bcm_gememalloc_ioctl.h

--- dag/v3d_library/src/bcm_gememalloc_ioctl.h
+++ dag/v3d_library/src/bcm_gememalloc_ioctl.h
@@ -26,7 +26,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ============================================================================ */
 
-#ifdef BRCM_USE_BMEM
+#ifdef USE_BMEM
 
 #ifndef _BMEM_IOCTL_H_
 #define _BMEM_IOCTL_H_
